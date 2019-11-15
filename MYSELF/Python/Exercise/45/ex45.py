e1af42ee22b6e0192ec6d1a534cba9b803288458
FOSS-ter <foss-ter@localhost.localdomain>
2019-11-14 22:16:21 -0500

    notes

---
+++ MYSELF/Python/Exercise/45/ex45.py
@@ -0,0 +1,6 @@
+# Create another text game
+# Requirements use more than one file so we need to import our own modules
+# One class per room
+# Another text game is quite boring, I will be skipping this excercise so I can move on from this
+# book faster and start coding an application.
+
