6e55e70654703fe334c47aa0aa1c51dccf5a260e
Jeremy Cunningham <jercun@amazon.com>
2019-12-04 13:25:23 -0500

    Finished ex 48

--- MYSELF/Python/Exercise/48/skeleton/ex48/lexicon.py
+++ MYSELF/Python/Exercise/48/skeleton/ex48/lexicon.py
@@ -1,12 +1,27 @@
-class lexicon(object):
-    def scan(raw):
-        words = raw.split
-        for word in words:
-            if word == "north" or word == "south" or word == "east" or word == "west" or word == "down":
-                print("test")
-
-newsentence = lexicon("This is a test south")
-lexicon.scan("north")
+def scan(raw):
+    words = raw.split()
+    directions = ("south", "north", "east", "west", "down", "up", "left", "right", "back")
+    verbs = ("go","kill","eat","attack","stab","chop","smack","punch")
+    stops = ("the","of","then","in","from","at","it")
+    nouns = ("door","bear","princess","cabinet")
+    numbers = str((list(range(0,99999))))
+    errors = ("om")
+    sentence = []
+    for word in words:
+        if word in directions:
+            sentence.append(("direction", word))
+        elif word in verbs:
+            sentence.append(("verb", word))
+        elif word in stops:
+            sentence.append(("stop", word))
+        elif word in nouns:
+            sentence.append(("noun", word))
+        elif word in numbers:
+            sentence.append(("number", int(word)))
+        else:
+            sentence.append(("error", word))
+    return sentence
+#print(scan("99 100"))
 # Code at a hight level
 # I think we dont need to worry about the user input, we juse need to focus on the unit test.
 # the end result of 48 and 49 is using the functions in 48 and combining it with the user input from 49 to turn user input into commands
